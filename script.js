626d92b389cfaae7d47634c8840fe5ca95fdccb1
Davy Nyakinyua <davynyakingua@gmail.com>
2025-08-20 13:48:36 +0300

    Add a class for nodes

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Balanced BST");
+
+class Node {
+    constructor (value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+    }
+}
+
+class Bbst {
+    constructor (arr){
+        this.root = null;
+    }
+
+    // method build tree to add nodes to tree
+    buildTree (){
+        
+    }
+}
